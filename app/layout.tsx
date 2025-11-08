21096b9221fb184ca5aafc5895ab17a8ae21f2f7
Quốc Chiến <123287600+quocchienn@users.noreply.github.com>
2025-11-08 22:28:45 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,8 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "CapCut Share",
+  description: "CapCut Share by quocchien",
 }
 
 export default function RootLayout({
